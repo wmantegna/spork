d04a79864ac6ec1b69429e0e9ea248f3c0551732
William Mantegna <wmantegna@gmail.com>
2014-11-04 21:43:26 -0500

    updates to homepage

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,7 +1,7 @@
 class VisitorsController < ApplicationController
 	
 	def index
-		if user_signed_in?
+		if user_signed_in? && !current_user.events.empty?
 			@upcomingEvents = current_user.events.where("date >= #{Date.today}").order("date DESC")
 			@pastEvents = current_user.events.limit(3).where("date < #{Date.today}").order("date DESC")
 		end
