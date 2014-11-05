d4a5739e064b2a3cd0a9e8fcd3913112eaaa9153
William Mantegna <wmantegna@gmail.com>
2014-11-04 21:37:42 -0500

    updates = updated homepage

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,4 +1,10 @@
 class VisitorsController < ApplicationController
+	
 	def index
+		if user_signed_in?
+			@upcomingEvents = current_user.events.where("date >= #{Date.today}").order("date DESC")
+			@pastEvents = current_user.events.limit(3).where("date < #{Date.today}").order("date DESC")
+		end
 	end
+
 end
